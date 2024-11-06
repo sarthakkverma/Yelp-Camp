969752b331e3d5dee98c720e3b8c1631b891c692
Sarthak Verma <sarthakverma0807@gmail.com>
2024-11-06 10:21:51 +0530

    Fixed a bug while creating a new campground

--- seeds/index.js
+++ seeds/index.js
@@ -20,7 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30)+10;
         const camp = new Campground({
-            author: '67291effa3af07fc2ba2f3c0',
+            author: '672af1e11f6e9f714034d497',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
